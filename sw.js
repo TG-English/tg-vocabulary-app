69dac03563e87b69e99543e05baf3d38aaeab12d
englishtg12-eng <english.tg.12@gmail.com>
2026-09-13 00:58:03 +0900

    Refresh cache for mixed prompt fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v13';
+const CACHE='tg-voca-v14';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
