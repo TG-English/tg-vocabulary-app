80643b2dd61526131e13eb7cc1b22083fd930555
englishtg12-eng <english.tg.12@gmail.com>
2026-09-13 00:37:37 +0900

    Refresh cache for cloud vocabulary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v9';
+const CACHE='tg-voca-v10';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
