c71a452a435ce2df047b06b504952a19b2ae5640
englishtg12-eng <english.tg.12@gmail.com>
2026-09-13 00:54:34 +0900

    Refresh cache for mixed tests

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v12';
+const CACHE='tg-voca-v13';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
