9e7f059b1c5f080a599f85b56353d196f61d877d
englishtg12-eng <english.tg.12@gmail.com>
2026-09-13 22:24:50 +0900

    Stabilize student sign-in and assigned exam loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v17';
+const CACHE='tg-voca-v18';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
