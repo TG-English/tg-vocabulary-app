4c739d0e016ebad30e082cc04a67b6f31a9d3818
englishtg12-eng <english.tg.12@gmail.com>
2026-09-14 12:03:47 +0900

    Increase TG header logo size

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v19';
+const CACHE='tg-voca-v20';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
